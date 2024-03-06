f955c856e23be92a7073ee9fe1541842a96adcbc
Harsh <harshsheth9909@gmail.com>
2024-03-07 00:03:52 +0530

    intial commit of native app

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import FormComponent from 'webpack2';
 
 export default function App() {
+  const handleSubmit = (formData) => {
+    console.log('Submitted Data:', formData);
+    // Add your submission logic here
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hi</Text>
+      <FormComponent onSubmit={handleSubmit}/>
       <StatusBar style="auto" />
     </View>
   );
